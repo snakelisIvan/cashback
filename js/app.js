63d0c733df4ab2ebf966a1e95819fc771851ab18
snakelis <snakelis@mail.ru>
2021-08-02 17:26:55 +0300

    calculator simple working version

--- js/app.js
+++ js/app.js
@@ -19,3 +19,31 @@ function calculateCashback(specialCategoryPurchases, otherCategoryPurchases) {
 
 const cashback = calculateCashback(5000, 10000);
 console.log(cashback);
+
+function handleSubmit(evt) {
+    evt.preventDefault();
+
+    const specialAmountInputEl = document.getElementById('special-amount-input');
+    const specialAmount = Number(specialAmountInputEl.value);
+    if (Number.isNaN(specialAmount)) {
+        // TODO: show error
+        return;
+    }
+    const otherAmountInputEl = document.getElementById('other-amount-input');
+    const otherAmount = Number(otherAmountInputEl.value);
+    if (!Number.isFinite(otherAmount)) {
+        // TODO: show error
+        return;
+    }
+
+    const result = calculateCashback(specialAmount, otherAmount);
+    const specialCashbackEl = document.getElementById('special-cashback');
+    specialCashbackEl.textContent = `${result.specialCategoryCashback} руб.`;
+    const otherCashbackEl = document.getElementById('other-cashback');
+    otherCashbackEl.textContent = `${result.otherCategoryCashback} руб.`;
+    const totalCashbackEl = document.getElementById('total-cashback');
+    totalCashbackEl.textContent = `${result.totalCashback} руб.`;
+}
+
+const formEl = document.getElementById('cashback-form');
+formEl.onsubmit = handleSubmit;
